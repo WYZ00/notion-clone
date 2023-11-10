4de4dc6b423ccc21dae23b66f0c2ed2f6390423f
WYZ00 <thanthtettin@gmail.com>
2023-11-10 20:17:32 +0630

    advanced file upload(delete,replace)

--- components/modals/cover-image-modal.tsx
+++ components/modals/cover-image-modal.tsx
@@ -29,7 +29,14 @@ export const CoverImageModal = () => {
     if (file) {
       setIsSubmitting(true);
       setFile(file);
-      const res = await edgestore.publicFiles.upload({ file });
+
+      const res = await edgestore.publicFiles.upload({
+        file,
+        options: {
+          replaceTargetUrl: coverImage.url,
+        },
+      });
+
       await update({
         id: params.documentId as Id<"documents">,
         coverImage: res.url,
